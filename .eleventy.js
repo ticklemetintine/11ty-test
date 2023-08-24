6fe0a0b1b72930e391ba1c1d6faf0c6f46363597
Justine <jsalangsang@blacksunplc.com>
2023-08-24 10:02:55 +0800

    'cloudcannon'

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const { DateTime } = require("luxon");
+const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
+const { wordCountCallback } = require("./site/js/wordCount");
+const MarkdownIt = require("markdown-it"),
+    md = new MarkdownIt({
+        html: true,
+    });
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.htmlTemplateEngine = "liquid";
+    eleventyConfig.addPlugin(
+        pluginBookshop({
+            bookshopLocations: ["component-library"],
+            pathPrefix: "",
+        })
+    );
+
+    return {
+        dir: {
+            input: 'site',
+            includes: '_includes',
+            data: "_data",
+            output: '_site',
+            pages: 'pages'
+        },
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk'
+    };
+};
