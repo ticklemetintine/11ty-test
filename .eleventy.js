189ea288224530dfd6963f5e10448df9d6b40f08
Justine <jsalangsang@blacksunplc.com>
2023-08-24 11:12:19 +0800

    'cloudcannon'

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const MarkdownIt = require("markdown-it"),
+    md = new MarkdownIt({
+        html: true,
+    });
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addFilter("markdownify", (markdown) => md.render(markdown));
     eleventyConfig.htmlTemplateEngine = "liquid";
     eleventyConfig.addPlugin(
         pluginBookshop({
